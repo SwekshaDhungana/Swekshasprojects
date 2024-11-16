39ccfee32c319cb41d556dc93c9f744ed311edd5
Sweksha Dhungana <115934697+SwekshaDhungana@users.noreply.github.com>
2024-11-16 17:44:24 +0545

    Add files via upload

---
+++ quiz_app/script.js
@@ -0,0 +1,64 @@
+const questions= [
+    {
+        question: "Who sang the title song for the latest Bond film, No Time to Die?",
+        answers: [
+            { Text: "Adele", correct: false},
+            { Text: "Sam smith", correct: false},
+            { Text: "Jungkook", correct: false},
+            { Text: "Billie Eilish", correct: true},
+
+        ]
+    },
+    {
+        question: "Which of the following is NOT a fruit??",
+        answers: [
+            { Text: "Tomatoes", correct: false},
+            { Text: "Avocadoes", correct: false},
+            { Text: "Apple", correct: false},
+            { Text: "Rhubarb", correct: true},
+
+        ]
+
+    },
+    {
+        question: "Where was the first example of paper money used?",
+        answers: [
+            { Text: "Turkey", correct: false},
+            { Text: "USA", correct: false},
+            { Text: "China", correct: true},
+            { Text: "India", correct: false},
+
+        ]
+    },
+    {
+        question: "Which of the following languages has the longest alphabet?",
+        answers: [
+            { Text: "Greek", correct: false},
+            { Text: "Russian", correct: true},
+            { Text: "Arabic", correct: false},
+            { Text: "Hindi", correct: true},
+
+        ]
+    },
+    {
+        question: "Which app has the most total users?",
+        answers: [
+            { Text: "Facebook", correct: false},
+            { Text: "Youtube", correct: false},
+            { Text: "Tiktok", correct: false},
+            { Text: "Instagram ", correct: true},
+
+        ]
+    },
+    {
+        question: "What is the strongest muscle in the human body?",
+        answers: [
+            { Text: "Heart", correct: false},
+            { Text: "Glutes", correct: false},
+            { Text: "Jaw", correct: true},
+            { Text: "Leg", correct: false},
+
+        ]
+    }
+];
+
